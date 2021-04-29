d2af87c60c396248fbcd95a0970bdd7cf3c7675b
Seonghong Min <sh.min@sk.com>
2021-04-29 13:52:42 +0900

    update docker build

--- dockerBuilder-chainz.sh
+++ dockerBuilder-chainz.sh
@@ -7,10 +7,14 @@ echo 'build version:' $BESU_VERSION
 
 IMG_TAG=$IMG_NAME':'$BESU_VERSION
 
-#BUILD_DATE='2021-04-21T18:26Z'
-BUILD_DATE=$(date +%Y-%m-%dT%H:%MZ)
-echo 'build date:' $BUILD_DATE
+#DATE='2021-04-21T18:26Z'
+DATE=$(date +%Y-%m-%dT%H:%MZ)
+echo 'build date:' $DATE
+
+GIT_REVISION=$(git rev-parse HEAD)
+REV=${GIT_REVISION:0:8}
+echo 'vcs(github):' $REV
 
 # 이미지 생성
 echo 'Docker build'
-docker build -t $IMG_TAG -f ./Dockerfile-chainz . --build-arg VERSION=$BESU_VERSION --build-arg BUILD_DATE=$BUILD_DATE
+docker build -t $IMG_TAG -f ./Dockerfile-chainz . --build-arg VERSION=$BESU_VERSION --build-arg BUILD_DATE=$DATE --build-arg VCS_REF=$REV
